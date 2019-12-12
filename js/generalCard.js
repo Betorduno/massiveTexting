8ca43e85bf4811e44cb83028f2d0e2a65ba5097f
Betorduno <alberto.rodriguez.davila@gmail.com>
2019-12-11 19:02:38 -0500

    cambios3

--- js/generalCard.js
+++ js/generalCard.js
@@ -1,4 +1,8 @@
+var i = 0;
+var txt = 'Text Messaging For Business Marketing.';
+var speed = 300;
 window.onload = function () {
+    typeWriter();
     new Vue({
         el: '#pricings',
         data: {
@@ -12,6 +16,7 @@ window.onload = function () {
                 req.send(null);
     
                 this.plans = JSON.parse(req.responseText);
+                console.log(this.plans)
 
                 this.plans = this.plans.map(plan => {
                     plan['class']="card pricing-plan-"+ plan.name +" pricing-card shadow mb-5 bg-white rounded"
@@ -25,4 +30,25 @@ window.onload = function () {
             this.getDate();
         }
     });    
+}
+
+
+function typeWriter() {
+    setLetters()
+}
+
+function setLetters () {
+    if (i <= txt.length) {
+        if (i === txt.length) {
+            setTimeout(function () {
+                document.getElementById("writer").innerHTML = ''
+                i = 0;
+
+                setLetters();
+            }, 2000);
+        }
+        document.getElementById("writer").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(setLetters, speed);
+    }
 }
