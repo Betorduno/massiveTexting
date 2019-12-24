ce0092621d750a24b8653c0fdd85d3a45c323c0d
Betorduno <alberto.rodriguez.davila@gmail.com>
2019-12-24 17:09:48 -0500

    cambio de color de la landing page, favicon, reference archivo de direccionamientos

---
+++ js/reference.js
@@ -0,0 +1,8 @@
+    var REGISTER='http://ec2-3-83-101-27.compute-1.amazonaws.com:3000/register';
+    var LOGIN ='http://ec2-3-83-101-27.compute-1.amazonaws.com:3000/login';
+
+     function start(){
+             document.getElementById("register").href= REGISTER;
+             document.getElementById("login").href= REGISTER;
+     }
+    
